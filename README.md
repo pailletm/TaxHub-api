6a4c367716cde1d4093060f2d1e7a02f6f068d06
amandine-sahl <amandine.sahl@gmail.com>
2016-06-07 17:44:50 +0200

    Création de la route /bibtaxons/

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Essaie de réécriture en flask de l'api de TaxHub.
 
 Pour le moment seul les routes /taxref sont fonctionnelles
+
+
+##Run
+./venv_flask/bin/python taxhubapi.py
--- apptax/bibtaxons/routes.py
+++ apptax/bibtaxons/routes.py
@@ -1,93 +1,66 @@
 #coding: utf8
 from flask import jsonify, Blueprint
-from . import models
+from flask import request
 
 from taxhubapi import init_app
 from taxhubapi import db
-
-from sqlalchemy import create_engine, MetaData, select, Table
-
-#For debug
-import pprint
+from ..utils import sqlalchemy as sqlautils
+from .models import BibTaxons, Taxref
+from sqlalchemy import create_engine, MetaData, select, Table, func
 
 adresses = Blueprint('bib_taxons', __name__)
 
 @adresses.route('/', methods=['GET'])
+@sqlautils.json_resp
 def get_bibtaxons():
-    results = models.BibTaxons.query.all()
-    # return jsonify({'bibtaxons': results})
-    return jsonify(data=[c.as_dict() for c in results])
+    bibTaxonColumns = BibTaxons.__table__.columns
+    taxrefColumns = Taxref.__table__.columns
+    parameters = request.args
+
+    q = db.session.query(BibTaxons)\
+        .join(BibTaxons.taxref)
+
+    #Traitement des parametres
+    limit = parameters.get('limit') if parameters.get('limit') else 100
+    offset = parameters.get('page') if parameters.get('page') else 0
+
+    for param in parameters:
+        if param in taxrefColumns:
+            col = getattr(taxrefColumns,param)
+            q = q.filter(col == parameters[param])
+        if param in bibTaxonColumns:
+            col = getattr(bibTaxonColumns,param)
+            q = q.filter(col == parameters[param])
+        elif param == 'ilikelatin':
+            q = q.filter(taxrefColumns.lb_nom.ilike(parameters[param]+'%'))
+    count= q.count()
+    results = q.limit(limit).all()
+    taxonsList = []
+    for r in results :
+        obj = r.as_dict()
+        obj['is_doublon'] = False
+
+        #Ajout des synonymes
+        #!! Deux façons d'écrire la requête avec des perfs très différentes mais alors vraiment
+        # q = select([BibTaxons.id_taxon])\
+        #     .where(func.taxonomie.find_cdref(BibTaxons.cd_nom) == func.taxonomie.find_cdref(obj['cd_nom']))\
+        #     .where(BibTaxons.id_taxon != obj['id_taxon'])
+        #@TODO réécrire la requête en mode subquery
+        q= "SELECT id_taxon \
+            FROM taxonomie.bib_taxons t \
+            JOIN taxonomie.taxref tx ON tx.cd_nom = t.cd_nom\
+            WHERE tx.cd_ref IN (\
+                SELECT  tx.cd_ref FROM taxonomie.taxref tx \
+                WHERE  tx.cd_nom = %d \
+            ) AND NOT t.id_taxon = %d" % (obj['cd_nom'],obj['id_taxon'])
+        results =db.session.connection().execute(q)
 
 
-#Test mix reflection/mapping db sur une table
-@adresses.route('/bib_taxons_taxref', methods=['GET'])
-def get_bibtaxonsXtaxref():
-    #connexion DB
-    engine = create_engine(init_app().config['SQLALCHEMY_DATABASE_URI'])
-    meta = MetaData(bind=engine)
+        if results.rowcount > 0 :
+            print(results.rowcount)
+            print([i.id_taxon for i in results])
+            obj['is_doublon'] = True
+            obj['synonymes'] = [i.id_taxon for i in results]
+        taxonsList.append(obj)
 
-    meta.reflect(schema='taxonomie', views=True)
-
-    tableBibTaxon = meta.tables['taxonomie.bib_taxons']
-    tableTaxref = meta.tables['taxonomie.taxref']
-
-    results = db.session.query(tableBibTaxon, tableTaxref)\
-        .join(tableTaxref, tableBibTaxon.columns.cd_nom==tableTaxref.columns.cd_nom)\
-        .all()
-
-    columns = [column.name for column in tableTaxref.columns] + [column.name for column in tableBibTaxon.columns]
-    rows = [
-        {
-            c: getattr(row, c)
-                for c in columns
-        }
-        for row in results
-    ]
-
-    return jsonify(rows)
-
-#Test reflecting db sur une table
-@adresses.route('/taxref', methods=['GET'])
-def get_taxref():
-    #connexion DB
-    engine = create_engine(init_app().config['SQLALCHEMY_DATABASE_URI'])
-    meta = MetaData(bind=engine)
-
-    meta.reflect(schema='taxonomie', views=True)
-
-    tableTaxref = Table('taxonomie.taxref', meta, autoload=True)
-    results = select([tableTaxref])\
-                .where(tableTaxref.columns.nom_complet.like('%Faga%'))\
-                .limit(5)\
-                .execute()
-
-    rows = [
-        {c.name: getattr(row, c.name)
-            for c in [column for column in tableTaxref.columns]
-            } for row in results
-        ]
-
-    return jsonify(rows)
-
-#Test reflecting db sur une vue
-@adresses.route('/plantae', methods=['GET'])
-def get_taxonPlantae():
-    #connexion DB
-    engine = create_engine(init_app().config['SQLALCHEMY_DATABASE_URI'])
-    meta = MetaData(bind=engine)
-
-    meta.reflect(schema='taxonomie', views=True)
-    print(meta.tables)
-    tableTaxref = meta.tables['taxonomie.v_bibtaxon_attributs_plantae']
-    # .where(tableTaxref.columns.nom_complet == 'Animalia')\
-    results = select([tableTaxref])\
-                .limit(5)\
-                .execute()
-
-    rows = [
-        {c.name: getattr(row, c.name)
-            for c in [column for column in tableTaxref.columns]
-            } for row in results
-        ]
-
-    return jsonify(rows)
+    return taxonsList
