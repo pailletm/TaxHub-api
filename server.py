4834c7647aa0db8962e9cd9bf425390683b0f256
amandine-sahl <amandine.sahl@gmail.com>
2016-06-24 16:10:22 +0200

    ajout des fichiers front dans le dépot - Création de la page index

--- server.py
+++ server.py
@@ -15,20 +15,24 @@ def init_app():
     db.init_app(app)
     app_globals['app'] = app
 
+
+    from apptax.index import routes
+    app.register_blueprint(routes, url_prefix='/')
+
     routesAuth = importlib.import_module("apptax.flaskmodule-UserHub-auth.routes")
-    app.register_blueprint(routesAuth.routes, url_prefix='/auth')
+    app.register_blueprint(routesAuth.routes, url_prefix='/api/auth')
 
     from apptax.taxonomie.routesbibtaxons import adresses
-    app.register_blueprint(adresses, url_prefix='/bibtaxons')
+    app.register_blueprint(adresses, url_prefix='/api/bibtaxons')
 
     from apptax.taxonomie.routestaxref import adresses
-    app.register_blueprint(adresses, url_prefix='/taxref')
+    app.register_blueprint(adresses, url_prefix='/api/taxref')
 
     from apptax.taxonomie.routesbibattributs import adresses
-    app.register_blueprint(adresses, url_prefix='/bibattributs')
+    app.register_blueprint(adresses, url_prefix='/api/bibattributs')
 
     from apptax.taxonomie.routesbiblistes import adresses
-    app.register_blueprint(adresses, url_prefix='/biblistes')
+    app.register_blueprint(adresses, url_prefix='/api/biblistes')
     return app
 
 
